a08296fbc4914a498936b6012f9af35ff12f0751
Angela Chung <mini.cchung@gmail.com>
2018-12-07 17:10:40 -0500

    survey pages

--- survey.php
+++ survey.php
@@ -12,32 +12,40 @@ $idVal=1;
 <!DOCTYPE html>
 <html>
 <head>
+  <link rel="stylesheet" type="text/css" href="css/survey.css" />
 </head>
 <body>
-  <form  action="survey-processing.php" method="post" id="add">
+  <img  id="image" src="images/sphoto.png">
+
+      <form  action="survey-processing.php" method="post" id="add">
 <?php
 while($row = $stmt->fetch()) {
         ?><div id="q<?=$idVal;?>" class="question">
-        <h2><?echo($idVal)?>/6</h2>
-        <h1>Q<?echo($idVal)?>:</h1><p> <?php echo($row["questions"]); ?></p>
+        <h2 id = "pagination"><?echo($idVal)?>/6</h2>
+        <h1 class="question">Q<?echo($idVal)?>:</h1>
+        <p class="question"><?php echo($row["questions"]);?></p>
 <?php
 
 if ($idVal == 1)
 {
   ?>
-<img class="Q1" id="a1" name="q1" data-value="beginner" src="buttons/take_survey.png">
-<img class="Q1" id="a1" name="q1" data-value="intermediate" src="buttons/take_survey.png">
-<img class="Q1" id="a1" name="q1" data-value="expert" src="buttons/take_survey.png">
+<br>
+<span id="set">
+<img class="Q1" id="a1" name="q1" data-value="beginner" src="buttons/SPbuttons/tier1/1.png">
+<img class="Q1" id="a1" name="q1" data-value="intermediate" src="buttons/SPbuttons/tier1/2.png">
+<img class="Q1" id="a1" name="q1" data-value="expert" src="buttons/SPbuttons/tier1/3.png">
+</span>
 <?php
 }elseif ($idVal == 2)
 {
 ?>
-<img class="Q2" id="a2" name="q2" data-value="happy" src="buttons/take_survey.png">
-<img class="Q2" id="a2" name="q2" data-value="mellow" src="buttons/take_survey.png">
-<img class="Q2" id="a2" name="q2" data-value="sleepy" src="buttons/take_survey.png">
-<img class="Q2" id="a2" name="q2" data-value="energetic" src="buttons/take_survey.png">
-<img class="Q2" id="a2" name="q2" data-value="creative" src="buttons/take_survey.png">
-<img class="Q2" id="a2" name="q2" data-value="focused" src="buttons/take_survey.png">
+<br>
+<img class="Q2" id="a2" name="q2" data-value="happy" src="buttons/SPbuttons/tier1/4.png">
+<img class="Q2" id="a2" name="q2" data-value="mellow" src="buttons/SPbuttons/tier1/5.png">
+<img class="Q2" id="a2" name="q2" data-value="sleepy" src="buttons/SPbuttons/tier1/6.png">
+<img class="Q2" id="a2" name="q2" data-value="energetic" src="buttons/SPbuttons/tier1/7.png">
+<img class="Q2" id="a2" name="q2" data-value="creative" src="buttons/SPbuttons/tier1/8.png">
+<img class="Q2" id="a2" name="q2" data-value="focused" src="buttons/SPbuttons/tier1/9.png">
 
 
 <?php
@@ -45,12 +53,13 @@ if ($idVal == 1)
 }elseif ($idVal == 3)
 {
 ?>
-<img  class="Q3" id="a3" name="q3" data-value="edible" src="buttons/take_survey.png">
-<img  class="Q3" id="a3" name="q3" data-value="pipe" src="buttons/take_survey.png">
-<img  class="Q3" id="a3" name="q3" data-value="bong" src="buttons/take_survey.png">
-<img  class="Q3" id="a3" name="q3" data-value="vape" src="buttons/take_survey.png">
-<img  class="Q3" id="a3" name="q3" data-value="pills" src="buttons/take_survey.png">
-<img  class="Q3" id="a3" name="q3" data-value="rolling paper" src="buttons/take_survey.png">
+<br>
+<img  class="Q3" id="a3" name="q3" data-value="edible" src="buttons/SPbuttons/tier1/10.png">
+<img  class="Q3" id="a3" name="q3" data-value="pipe" src="buttons/SPbuttons/tier1/11.png">
+<img  class="Q3" id="a3" name="q3" data-value="bong" src="buttons/SPbuttons/tier1/12.png">
+<img  class="Q3" id="a3" name="q3" data-value="vape" src="buttons/SPbuttons/tier1/13.png">
+<img  class="Q3" id="a3" name="q3" data-value="pills" src="buttons/SPbuttons/tier1/14.png">
+<img  class="Q3" id="a3" name="q3" data-value="rolling paper" src="buttons/SPbuttons/tier1/15.png">
 
 
 
@@ -58,24 +67,27 @@ if ($idVal == 1)
 }elseif ($idVal == 4)
 {
 ?>
-<img  class="Q4" id="a4" name="q4" data-value="yes" src="buttons/take_survey.png">
-<img  class="Q4" id="a4" name="q4" data-value="no" src="buttons/take_survey.png">
+<br>
+<img  class="Q4" id="a4" name="q4" data-value="yes" src="buttons/SPbuttons/tier1/16.png">
+<img  class="Q4" id="a4" name="q4" data-value="no" src="buttons/SPbuttons/tier1/17.png">
 
 
 <?php
 }elseif ($idVal == 5)
 {
 ?>
-<img  class="Q5" id="a5" name="q5" data-value="yes" src="buttons/take_survey.png">
-<img  class="Q5" id="a5" name="q5" data-value="no" src="buttons/take_survey.png">
+<br>
+<img  class="Q5" id="a5" name="q5" data-value="yes" src="buttons/SPbuttons/tier1/18.png">
+<img  class="Q5" id="a5" name="q5" data-value="no" src="buttons/SPbuttons/tier1/19.png">
 
 
 <?php
 }elseif ($idVal == 6)
 {
 ?>
-<img  class="Q6" id="a6" name="q6" data-value="yes" src="buttons/take_survey.png">
-<img  class="Q6" id="a6" name="q6" data-value="no" src="buttons/take_survey.png">
+<br>
+<img  class="Q6" id="a6" name="q6" data-value="yes" src="buttons/SPbuttons/tier1/20.png">
+<img  class="Q6" id="a6" name="q6" data-value="no" src="buttons/SPbuttons/tier1/21.png">
 
 <?php
 }
@@ -97,11 +109,11 @@ if ($idVal == 1)
 
 ?>
 
-<br><br>
 <p id="Prev">Prev</p>
 <p id="Next">Next</p>
 <input type="submit" >
 </form>
+
 <!-- <script>
 
 var currentQ = 1;
