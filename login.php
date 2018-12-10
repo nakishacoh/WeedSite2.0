a7c443462d8b3083a1432d0c83b187efb35aeeff
Angela Chung <mini.cchung@gmail.com>
2018-12-10 14:43:29 -0500

    photos
    sdfsdfd

--- login.php
+++ login.php
@@ -3,25 +3,18 @@ include('header.php')
 ?>
 <link rel="stylesheet" type="text/css" href="css/login.css" />
 <img src="images/bar1.png">
-
-  <h1>LOGIN</h1>
-  <br>
-  <br>
+  <h1>Welcome back</h1>
+  <h2>Log in</h2>
 
   <form class="form" method='POST' action='process-login.php'>
-      
-      Username: <input type='text' name="username" />
-      <br>
-      Password: <input type='text' name="password" />
-      <br>
-      <img id="login" src="buttons/login.png">
-    <!-- <input type='submit'> -->
+
+      <div id = "email">E-mail: <input type='email' name="username" placeholder="example@example.com" /></div>
+
+      <div id = "password">Password: <input type='password' name="password" placeholder="Your password"/></div>
+
+      <!-- <button><img id="login" type ='submit' src="buttons/login.png"></button> -->
+    <input id = "button" type='submit' text="log in">
   </form>
-
-  <br>
-  <br>
-  <br>
-
 
   <?php
   include('footer.php')
