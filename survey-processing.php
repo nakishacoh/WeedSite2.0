032b581f1e9ae646df715d4a60f449d8481edba8
Chris-ric-browne <42975420+Chris-ric-browne@users.noreply.github.com>
2018-12-10 16:54:51 -0500

    Update survey-processing.php

--- survey-processing.php
+++ survey-processing.php
@@ -1,16 +1,81 @@
 <?php
 $decodedDataObject =  json_decode($_POST["data"]);
 
+$strain="";
+if ($decodedDataObject->a1=="beginner" &&
+    $decodedDataObject->a2=="happy" || $decodedDataObject->a2=="creative" &&
+    $decodedDataObject->a3=="edible" || $decodedDataObject->a3=="pipe" || $decodedDataObject->a3=="bong" ||
+    $decodedDataObject->a3=="vape" || $decodedDataObject->a3=="pills" || $decodedDataObject->a3=="rolling paper")
 
-if ($decodedDataObject->a1="beginner" &&
-    $decodedDataObject->a2="happy" &&
-    $decodedDataObject->a3="edible" &&
-    $decodedDataObject->a1="yes" &&
-    $decodedDataObject->a1="yes" &&
-    $decodedDataObject->a1="yes")
 {
-  $strain="sativa";
+  $strain="Purple Haze";
  }
+ elseif ($decodedDataObject->a1=="beginner" &&
+     $decodedDataObject->a2=="mellow" ||  $decodedDataObject->a2=="focused" &&
+     $decodedDataObject->a3=="edible" || $decodedDataObject->a3=="pipe" || $decodedDataObject->a3=="bong" ||
+     $decodedDataObject->a3=="vape" || $decodedDataObject->a3=="pills" || $decodedDataObject->a3=="rolling paper")
+ {
+    $strain="Chocolope";
+ }
+ elseif ($decodedDataObject->a1="beginner" &&
+     $decodedDataObject->a2=="sleepy" &&
+     $decodedDataObject->a3=="edible" || $decodedDataObject->a3=="pipe" || $decodedDataObject->a3=="bong" ||
+     $decodedDataObject->a3=="vape" || $decodedDataObject->a3=="pills" || $decodedDataObject->a3=="rolling paper")
+ {
+    $strain="Obama Kush";
+ }
+ elseif ($decodedDataObject->a1=="beginner" &&
+     $decodedDataObject->a2=="energetic" &&
+     $decodedDataObject->a3=="edible" || $decodedDataObject->a3=="pipe" || $decodedDataObject->a3=="bong" ||
+     $decodedDataObject->a3=="vape" || $decodedDataObject->a3=="pills" || $decodedDataObject->a3=="rolling paper")
+ {
+    $strain="Bubba Kush";
+ }
+ elseif ($decodedDataObject->a1=="intermediate" &&
+     $decodedDataObject->a2=="happy" || $decodedDataObject->a2=="creative" &&
+     $decodedDataObject->a3=="edible" || $decodedDataObject->a3=="pipe" || $decodedDataObject->a3=="bong" ||
+     $decodedDataObject->a3=="vape" || $decodedDataObject->a3=="pills" || $decodedDataObject->a3=="rolling paper")
+ {
+    $strain="Super Skunk";
+ }
+ elseif ($decodedDataObject->a1=="intermediate" &&
+     $decodedDataObject->a2=="sleepy" || $decodedDataObject->a2=="mellow" &&
+     $decodedDataObject->a3=="edible" || $decodedDataObject->a3=="pipe" || $decodedDataObject->a3=="bong" ||
+     $decodedDataObject->a3=="vape" || $decodedDataObject->a3=="pills" || $decodedDataObject->a3=="rolling paper")
+ {
+    $strain="Moby Dick";
+ }
+
+ elseif ($decodedDataObject->a1=="intermediate" &&
+     $decodedDataObject->a2=="energetic" || $decodedDataObject->a2=="focused" &&
+     $decodedDataObject->a3=="edible" || $decodedDataObject->a3=="pipe" || $decodedDataObject->a3=="bong" ||
+     $decodedDataObject->a3=="vape" || $decodedDataObject->a3=="pills" || $decodedDataObject->a3=="rolling paper")
+ {
+    $strain="Dutch Treat";
+ }
+ elseif ($decodedDataObject->a1=="expert" &&
+     $decodedDataObject->a2=="happy" || $decodedDataObject->a2=="creative" &&
+     $decodedDataObject->a3=="edible" || $decodedDataObject->a3=="pipe" || $decodedDataObject->a3=="bong" ||
+     $decodedDataObject->a3=="vape" || $decodedDataObject->a3=="pills" || $decodedDataObject->a3=="rolling paper")
+ {
+    $strain="Chernobyl";
+ }
+ elseif ($decodedDataObject->a1=="expert" &&
+     $decodedDataObject->a2=="energetic" || $decodedDataObject->a2=="focused" &&
+     $decodedDataObject->a3=="edible" || $decodedDataObject->a3=="pipe" || $decodedDataObject->a3=="bong" ||
+     $decodedDataObject->a3=="vape" || $decodedDataObject->a3=="pills" || $decodedDataObject->a3=="rolling paper")
+ {
+    $strain="ACDC";
+ }
+ elseif ($decodedDataObject->a1=="expert" &&
+     $decodedDataObject->a2=="sleepy" || $decodedDataObject->a2=="mellow" &&
+     $decodedDataObject->a3=="edible" || $decodedDataObject->a3=="pipe" || $decodedDataObject->a3=="bong" ||
+     $decodedDataObject->a3=="vape" || $decodedDataObject->a3=="pills" || $decodedDataObject->a3=="rolling paper")
+ {
+    $strain="Strawberry Cough ";
+ }
+
+
 
 
  $dsn = "mysql:host=localhost;dbname=browne9_Weedsite;charset=utf8mb4";
