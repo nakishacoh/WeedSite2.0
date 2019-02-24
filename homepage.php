a0f638f73fadea3b71226de4ec82cff81a259780
Chris-ric-browne <42975420+Chris-ric-browne@users.noreply.github.com>
2019-02-24 16:20:25 -0500

    commit

--- homepage.php
+++ homepage.php
@@ -1,31 +1,72 @@
-<!-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
- <link rel="stylesheet" href="css/homepage.css" />
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="stylesheet" type="text/css" href="css/bootstrap-social.css" />
+  <link rel="stylesheet" type="text/css" href="css/bootstrap-social.scss" />
+  <link rel='stylesheet' media='screen and (min-width: 480px)' href='css/homepage.css' />
+  <link rel="stylesheet" type="text/css" href="css/mobile-homepage.css" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous"> -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 </head>
 <body>
  <header>
-   <nav >
+   <!-- <nav >
      <ul class="navigation">
-       <li><a href=""><img src="images/logo.png"></a></li>
-       <li><a href="">Login</a></li>
-       <li><a href="">Sign Up</a></li>
-       <li><a href="">Home</a></li>
-       <li><a href="">About</a></li>
-       <li><a href="">User Account</a></li>
+       <li><a href="homepage.php">Home</a></li>
+       <li><a href="survey.php">Survey</a></li>
+       <li><a href="learnmore.php">Learn More</a></li>
+       <li><a href="register.php">Register</a></li>
+       <li><a href="login.php">Login</a></li>
+       <li><a href="aboutpage.php">About</a></li>
      </ul>
- </nav>
- </header> -->
+ </nav> -->
+ <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <a class="navbar-brand" href="index.php"><img class="heading "src="images/logo.png"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav ml-auto ">
+      <li class="nav-item">
+        <a class="nav-link" href="homepage.php">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="survey.php">Survey</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="learnmore.php">Learn More</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="register.php">Register</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="login.php">Login</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="aboutpage.php">About</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+ </header>
 
- <?php
-   include('header.php')
- ?>
-<link rel="stylesheet" type="text/css" href="css/homepage.css" />
+
+
+
 
    <!--Survey-->
 <div id="top">
  <!--  weed photo-->
-     <img src="images/image_1.png"/>
+
+    <h1 class="hp1"><b>Take our free survey <br> to discover a strain <br> most desirable for you</b></h1>
+     <img class="img-responsive hp_1" src="images/hp_1.png"/>
  <!--  weed photo end-->
 
        <!-- <div class="surveybtn"> -->
@@ -37,31 +78,62 @@
 
  <!--Site Explanation-->
 
-<div>
- <img src="images/image_4.png"/>
+<div class="text_black">
+ <!-- <img src="images/image_4.png"/> -->
+ <p class="text">We know searching for canniabis strains can get overwhelming<br>
+     with teh hundreds of varieties avaliable to purchase<br><br>
+     And since <b>time</b> and <b>money</b> is valuable<br><br>
+     We want your cannabis experience to be <b>safe</b> and<br>
+     <b>enjoyable</b>, so we created a questionaire to reveal strains<br>
+     best for recreation.</p>
 </div>
 
  <!--Cannibis Info -->
 <div id="mid">
-   <img src="images/image_2.png"/>
+  <h1 class="hp2">Discover cannibis <br> types and products</h1>
+   <img class="img-responsive hp_1" src="images/hp_2.png"/>
    <a class="learnB" href="#"><img src="buttons/learn_more.png">
    </a>
 </div>
  <!-- About Us -->
 <div id="last">
- <img src="images/image_3.png"/>
+  <h1 class="hp3">About us</h1>
+  <p class="hp3_2">Curious about who <br> we are?</p>
+ <img class="img-responsive hp_1" src="images/hp_3.png"/>
  <a class="checkus" href="aboutpage.php">
    <img src="buttons/check_us_out.png">
  </a>
 </div>
 
-<?php
- include('footer.php')
-?>
 
-<!--
- <footer>
+
+<div>
+ <footer class="footer">
+
+<!-- <a class="btn btn-social-icon btn-twitter">
+<span class="fa fa-twitter"></span>
+</a> -->
+
+<i class="fa fa-facebook-f" style="font-size:40px;color:white"></i>
+<i class="fa fa-instagram" style="font-size:40px;color:white"></i>
+<i class="fa fa-github" style="font-size:40px;color:white"></i>
+<i class="fa fa-twitter" style="font-size:40px;color:white"></i>
+
+
+
+<!-- <a class="btn btn-social-icon btn-github">
+<span class="fa fa-github"></span>
+</a>
+
+<a class="btn btn-social-icon btn-google">
+<span class="fa fa-google"></span>
+</a>
+
+<a class="btn btn-social-icon btn-linkedin">
+<span class="fa fa-linkedin"></span>
+</a> -->
 
  </footer>
+</div>
 </body>
-</html> -->
+</html>
